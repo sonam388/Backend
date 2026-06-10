93381b3582a445557eab1fae054dc79c0355ce13
Rishavchourasiya <rishavchourasiya945@gmail.com>
2026-06-10 17:32:04 +0530

    checking the apis routes

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: (process.env.CORS_ORIGIN || "http://localhost:5173,http://localhost:5173")
+    origin: (process.env.CORS_ORIGIN ||  "http://localhost:5173",
+      "https://dayodaygaushalatendukheda.org",
+      "https://www.dayodaygaushalatendukheda.org")
       .split(",")
       .map((origin) => origin.trim()),
     credentials: true,
